4c0d4612b2ba475791331b3df679d6f4a6813999
Maciek Krawiec <maciek.krawiec@zoho.com>
2017-10-29 18:41:08 +0100

    Pass arguments to zypper

--- zigzag-languages/install-languages.sh
+++ zigzag-languages/install-languages.sh
@@ -7,4 +7,4 @@ lang_short=${LANG:0:2}
 lang_full=${LANG:0:5}
 recommended=$(zypper -n packages -u --recommended)
 awk_filter="/-lang |-$lang_short |-$lang_full /{print \$4}"
-echo "$recommended" | awk "$awk_filter" | tr '\n' ' ' | xargs sudo zypper install
+echo "$recommended" | awk "$awk_filter" | tr '\n' ' ' | xargs sudo zypper install $@
