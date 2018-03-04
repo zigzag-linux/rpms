a657cf871d8a61b9e6741ae2afecf77d7284225d
Maciek Krawiec <maciek.krawiec@zoho.com>
2018-03-04 11:25:08 +0100

    Avoid refreshing zypper repos

--- zigzag-tools/zigzag-languages.sh
+++ zigzag-tools/zigzag-languages.sh
@@ -6,9 +6,10 @@
 lang_short=${LANG:0:2}
 lang_full=${LANG:0:5}
 
+zypper="zypper --no-refresh"
 awk_filter="/-lang |-$lang_short |-$lang_full /{print \$4}"
-to_install=$(zypper -n packages -u --recommended | awk "$awk_filter" | tr '\n' ' ')
+to_install=$($zypper packages -u --recommended | awk "$awk_filter" | tr '\n' ' ')
 
 if [ -n "$to_install" ]; then
-    sudo zypper install $@ $to_install
+    sudo $zypper install $@ $to_install
 fi
