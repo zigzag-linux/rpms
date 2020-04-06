c0379e2053b4d320f291f59c2c41c3cfa8885b19
Maciek Krawiec <maciek.krawiec@zoho.com>
2020-04-06 21:03:18 +0200

    Revamp zigzag-languages tool

--- zigzag-tools/zigzag-languages.sh
+++ zigzag-tools/zigzag-languages.sh
@@ -1,15 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Install needed translation packages
 # Usage: zigzag-languages [-y]
 
-lang_short=${LANG:0:2}
-lang_full=${LANG:0:5}
+declare -r FILE_INSTALLED=$(mktemp)
+declare -r FILE_LANGPACKS=$(mktemp)
+declare -r ZYPPER='zypper --no-refresh'
 
-zypper="zypper --no-refresh"
-awk_filter="/-lang |-$lang_short |-$lang_full /{print \$4}"
-to_install=$($zypper packages -u --recommended | awk "$awk_filter" | tr '\n' ' ')
+export LC_ALL="C"
+
+rpm -qa --qf "%{NAME}-lang\n" |
+    sort >$FILE_INSTALLED
+
+$ZYPPER -x search -u "*-lang"      |
+    grep -oP '(?<=name=").*?(?=")' |
+    sort >$FILE_LANGPACKS
+
+to_install=$(comm -12 $FILE_INSTALLED $FILE_LANGPACKS)
+rm $FILE_INSTALLED $FILE_LANGPACKS
 
 if [ -n "$to_install" ]; then
-    sudo $zypper install $@ $to_install
+    sudo $ZYPPER install $@ $to_install
 fi
