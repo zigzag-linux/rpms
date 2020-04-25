5ce36fc0c4dc0a4728cbd0c879f50a4ba066f555
Maciek Krawiec <maciek.krawiec@zoho.com>
2020-04-25 12:18:39 +0200

    Revert "Revamp zigzag-languages tool"
    This reverts commit c0379e2053b4d320f291f59c2c41c3cfa8885b19.

--- zigzag-tools/zigzag-languages.sh
+++ zigzag-tools/zigzag-languages.sh
@@ -1,24 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Install needed translation packages
 # Usage: zigzag-languages [-y]
 
-declare -r FILE_INSTALLED=$(mktemp)
-declare -r FILE_LANGPACKS=$(mktemp)
-declare -r ZYPPER='zypper --no-refresh'
+lang_short=${LANG:0:2}
+lang_full=${LANG:0:5}
 
-export LC_ALL="C"
-
-rpm -qa --qf "%{NAME}-lang\n" |
-    sort >$FILE_INSTALLED
-
-$ZYPPER -x search -u "*-lang"      |
-    grep -oP '(?<=name=").*?(?=")' |
-    sort >$FILE_LANGPACKS
-
-to_install=$(comm -12 $FILE_INSTALLED $FILE_LANGPACKS)
-rm $FILE_INSTALLED $FILE_LANGPACKS
+zypper="zypper --no-refresh"
+awk_filter="/-lang |-$lang_short |-$lang_full /{print \$4}"
+to_install=$($zypper packages -u --recommended | awk "$awk_filter" | tr '\n' ' ')
 
 if [ -n "$to_install" ]; then
-    sudo $ZYPPER install $@ $to_install
+    sudo $zypper install $@ $to_install
 fi
